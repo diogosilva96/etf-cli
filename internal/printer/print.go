eefd43a0669aae82dbbe8c5ab233ce09e6052a51
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-13 19:11:55 +0100

    Add documentation to print module

--- internal/printer/print.go
+++ internal/printer/print.go
@@ -7,14 +7,20 @@ const (
 	warningColor        = "\033[1;33m%s\033[0m"
 )
 
-func PrintError(format string, a ...any) {
-	fmt.Printf(errorColor, fmt.Sprintf(format, a...))
+// PrintError formats an error according to a format specifier and writes it to standard output.
+// It returns the number of bytes written and any write error encountered.
+func PrintError(format string, a ...any) (int, error) {
+	return fmt.Printf(errorColor, fmt.Sprintf(format, a...))
 }
 
-func PrintWarning(format string, a ...any) {
-	fmt.Printf(warningColor, fmt.Sprintf(format, a...))
+// PrintWarning formats a warning according to a format specifier and writes it to standard output.
+// It returns the number of bytes written and any write error encountered.
+func PrintWarning(format string, a ...any) (int, error) {
+	return fmt.Printf(warningColor, fmt.Sprintf(format, a...))
 }
 
-func Print(format string, a ...any) {
-	fmt.Printf(fmt.Sprintf(format, a...))
+// Print formats according to a format specifier and writes it to standard output.
+// It returns the number of bytes written and any write error encountered.
+func Print(format string, a ...any) (int, error) {
+	return fmt.Printf(fmt.Sprintf(format, a...))
 }
