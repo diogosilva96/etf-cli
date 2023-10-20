f9173b11e6615bee2ab4d133c90604e880c7fb63
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-20 23:29:13 +0100

    Remove unecessary function

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,10 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/diogosilva96/etf-scraper/cmd/config"
-	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -37,33 +34,4 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(config.InitConfig)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	fmt.Printf("")
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigType(config.ConfigType)
-		viper.SetConfigName(config.ConfigName)
-		viper.SetDefault("etfs", []string{"VWCE.DE", "VWCE.MI"})
-		viper.SafeWriteConfig()
-	}
-
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("Config file changed:", e.Name)
-	})
-	viper.WatchConfig()
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Fprintln(os.Stderr, "No config file could be found.\n Falling back to default config file:", viper.ConfigFileUsed())
-	}
 }
