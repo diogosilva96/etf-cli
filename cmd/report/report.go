3f80a3905e2dd31dffbba75459bdacac90c4701d
Diogo Silva <diogo_silva_96@hotmail.com>
2023-10-23 21:01:01 +0100

    Move some code form report.go to generate.go

--- cmd/report/report.go
+++ cmd/report/report.go
@@ -1,12 +1,5 @@
 package report
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/diogosilva96/etf-cli/cmd/scraper"
-)
-
 // EtfReport repesents a report containg etf data.
 type EtfReport struct {
 	Symbol               string
@@ -18,54 +11,4 @@ type EtfReport struct {
 type EtfIntervalReport struct {
 	IntervalInDays                     int
 	MinPrice, MaxPrice, IntervalChange float32
-}
-
-// GenerateReport generates a report based on the provided etf struct.
-func GenerateReport(etf scraper.Etf) *EtfReport {
-
-	// set the number of days for the interval reports (e.g., last 5, 30 & 60 days)
-	intervals := []int{5, 30, 60}
-	report := &EtfReport{
-		Symbol:          etf.Symbol,
-		CurrentPrice:    etf.Price,
-		Change:          (etf.Price - etf.History[1].Price),
-		IntervalReports: make([]EtfIntervalReport, 0),
-	}
-
-	for _, interval := range intervals {
-		intervalReport, err := generateIntervalReport(etf, interval)
-		if err != nil {
-			// do something, or is it ok to ignore?
-			continue
-		}
-		report.IntervalReports = append(report.IntervalReports, *intervalReport)
-	}
-
-	return report
-}
-
-func generateIntervalReport(etf scraper.Etf, numberOfDays int) (*EtfIntervalReport, error) {
-	report := &EtfIntervalReport{
-		IntervalInDays: numberOfDays,
-		MaxPrice:       etf.Price,
-		MinPrice:       etf.Price,
-	}
-
-	historySize := len(etf.History)
-	if historySize-1 < numberOfDays {
-		return nil, errors.New(fmt.Sprintf("The etf history is only '%v' days long.", historySize))
-	}
-	for i, h := range etf.History[:numberOfDays] {
-		if h.Price > report.MaxPrice {
-			report.MaxPrice = h.Price
-		}
-		if h.Price < report.MinPrice {
-			report.MinPrice = h.Price
-		}
-		if i == numberOfDays-1 {
-			report.IntervalChange = (etf.Price - h.Price) - 1
-		}
-	}
-
-	return report, nil
 }
