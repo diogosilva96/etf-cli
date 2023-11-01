71f888f6f3ec7764e81886bf012a2fdfa06bd13e
Diogo Silva <diogo_silva_96@hotmail.com>
2023-11-01 10:08:48 +0000

    Refactor remove command to allow for remove an etf based on its index in the configuration (instead of its symbol)

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,28 +1,38 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/diogosilva96/etf-cli/internal/config"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 // removeCmd represents the remove command
 var removeCmd = &cobra.Command{
 	Use:   "remove",
-	Short: "Removes an existing ETF from the configuration.",
-	Long: `Removes an existing ETF from the configuration.
-	
+	Short: "Removes an existing ETF from the configuration based on the specified index.",
+	Long: `Removes an existing ETF from the configuration based on the specified index.
+
 	When the 'report' command is used the data from the removed ETF will no longer be displayed.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		etf := args[0]
-		err := config.RemoveEtf(etf)
+		idxStr := args[0]
+		idx, err := strconv.Atoi(idxStr)
 		if err != nil {
-			cmd.PrintErr(err)
+			cmd.PrintErrf("'%s' is not a valid number.\n", idxStr)
 			return
 		}
-		cmd.Printf(fmt.Sprintf("'%s' successfully removed!", etf))
+		etfs := config.ListEtfs()
+		for i, etf := range etfs {
+			if idx == i+1 {
+				err := config.RemoveEtf(etf)
+				if err != nil {
+					cmd.PrintErr(err)
+					return
+				}
+				cmd.Printf("'%s' successfully removed!", etf)
+				return
+			}
+		}
 	},
 }
 
