adef2c698af0e7993a628da541d20cee8eef2057
Diogo Silva <diogo_silva_96@hotmail.com>
2023-11-22 19:07:10 +0000

    Fix docs

--- internal/data/etf.go
+++ internal/data/etf.go
@@ -1,13 +1,13 @@
 package data
 
-// Represents an Etf
+// Etf Represents an Etf
 type Etf struct {
 	Symbol  string
 	Price   float32
 	History []EtfHistory
 }
 
-// Represents a single history element of an ETF
+// EtfHistory represents a single history element of an ETF
 type EtfHistory struct {
 	Date  string
 	Price float32
