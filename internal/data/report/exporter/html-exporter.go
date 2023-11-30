2f1512a627b68dce0d10e16196f85adb4d92faed
Diogo Silva <diogo_silva_96@hotmail.com>
2023-11-30 20:17:56 +0000

    Add chartData normalization to templateData (html-exporter)

--- internal/data/report/exporter/html-exporter.go
+++ internal/data/report/exporter/html-exporter.go
@@ -17,7 +17,7 @@ func NewHtmlReportExporter() ReportExporter {
 	return htmlReportExporter{}
 }
 
-// Export exports the reports to a html file with the specified file path.
+// Export exports the Reports to a html file with the specified file path.
 func (e htmlReportExporter) Export(reports []report.EtfReport, filePath string) error {
 	err := utils.CreateFoldersIfNotExist(filePath)
 	if err != nil {
@@ -32,7 +32,7 @@ func (e htmlReportExporter) Export(reports []report.EtfReport, filePath string) 
 		return err
 	}
 
-	templateData := createHtmlTemplateData(time.Now(), reports)
+	templateData := createTemplateData(time.Now(), reports)
 	err = tmpl.Execute(file, templateData)
 	if err != nil {
 		return err
@@ -40,11 +40,61 @@ func (e htmlReportExporter) Export(reports []report.EtfReport, filePath string) 
 	return nil
 }
 
-func createHtmlTemplateData(date time.Time, reports []report.EtfReport) htmlTemplateData {
-	return htmlTemplateData{Date: date, Reports: reports}
+func createTemplateData(date time.Time, reports []report.EtfReport) templateData {
+	var symbols []string
+	var entries []chartEntry
+	for _, r := range reports {
+		symbols = append(symbols, r.Symbol)
+		entries = createChartEntries(r, entries)
+	}
+	return templateData{
+		Date:    date,
+		Reports: reports,
+		ChartData: chartData{
+			symbols: symbols,
+			entries: entries,
+		},
+	}
 }
 
-type htmlTemplateData struct {
-	Date    time.Time
-	Reports []report.EtfReport
+func createChartEntries(r report.EtfReport, entries []chartEntry) []chartEntry {
+	for _, h := range r.History {
+		found, entry := findChartEntry(entries, h.Date)
+		var prices []float32
+		if !found {
+			entry = &chartEntry{}
+			entry.date = h.Date
+			entry.prices = append(prices, h.Price)
+			entries = append(entries, *entry)
+			continue
+		}
+		entry.prices = append(entry.prices, h.Price)
+		entries = append(entries, *entry)
+	}
+	return entries
+}
+
+func findChartEntry(entries []chartEntry, date time.Time) (bool, *chartEntry) {
+	for _, e := range entries {
+		if e.date == date {
+			return true, &e
+		}
+	}
+	return false, nil
+}
+
+type templateData struct {
+	Date      time.Time
+	Reports   []report.EtfReport
+	ChartData chartData
+}
+
+type chartData struct {
+	symbols []string
+	entries []chartEntry
+}
+
+type chartEntry struct {
+	date   time.Time
+	prices []float32
 }
